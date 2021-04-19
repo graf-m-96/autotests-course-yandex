c1c678713ce9bb872c37186eb3936cd0f631c714
graf-m <graf.m.96@yandex.ru>
2021-04-19 23:29:56 +0500

    autotests-task-04

---
+++ autotests-task-04/page-object.js
@@ -0,0 +1,13 @@
+// Здесь опиши используемые селекторы
+
+module.exports = {
+    github: {
+        login: '#login_field',
+        password: '#password',
+        signInBtn: '[name=commit]',
+        reauthorization: '#js-oauth-authorize-btn'
+    },
+    kilogram: {
+        nickname: '._3Hw2ZjDdZIklo5kekID6jw + span'
+    }
+};
